4bfb7e5ed0be95b2f00ba12204028d8b04fc854f
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-12-28 14:52:21 -0800

    Allow Games to create Cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,15 @@
 class Card < ApplicationRecord
   belongs_to :game
+
+  CARD_CONTENT = [
+    'Pikachu',
+    'Bulbasaur',
+    'Charmander',
+    'Squirtle',
+    'Eevee',
+    'Chikorita',
+    'Cyndaquil',
+    'Totodile',
+    'Marill'
+  ]
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :cards
+  after_create :generate_cards
   
   validates :user_id, presence: true
   validates :player_one_id, presence: true
@@ -11,5 +12,11 @@ class Game < ApplicationRecord
 
   def validate_player_uniqueness
     errors.add(:base, "Games must have different Players") if player_one_id == player_two_id
+  end
+
+  def generate_cards
+    (Card::CARD_CONTENT*2).shuffle.each_with_index do |content, index|
+      self.cards.create(content: content, position: index)
+    end
   end
 end
