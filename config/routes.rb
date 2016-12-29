bfef6b27e02a5c634612d03dd1741d184f1c8c3d
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-12-29 11:47:14 -0800

    Stub user authentication for controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   
   root 'application#hello'
+
+  resources :users do
+    get '/games', to: 'games#index'
+  end
   resources :games do
   end
   patch 'games/:id/match_cards', to: 'games#match_cards', as: 'match_cards'
