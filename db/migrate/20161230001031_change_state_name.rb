c44c180c184a457e8955801ef9fcf9026f5b4b6f
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-12-29 19:49:54 -0800

    remove extra param from old migration

--- db/migrate/20161230001031_change_state_name.rb
+++ db/migrate/20161230001031_change_state_name.rb
@@ -1,5 +1,5 @@
 class ChangeStateName < ActiveRecord::Migration[5.0]
   def change
-    rename_column :cards, :state, :revealed, default: false
+    rename_column :cards, :state, :revealed
   end
 end
