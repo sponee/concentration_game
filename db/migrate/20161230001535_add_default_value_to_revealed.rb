4417ff3a12e447000f755dfd6d6613114f1f2bf5
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-12-29 20:09:19 -0800

    update syntax of migration to comply with postgresql

--- db/migrate/20161230001535_add_default_value_to_revealed.rb
+++ db/migrate/20161230001535_add_default_value_to_revealed.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToRevealed < ActiveRecord::Migration[5.0]
   def change
-    change_column :cards, :revealed, :boolean, :default => false
+    change_column :cards, :revealed, 'boolean USING revealed::boolean', :default => false
   end
 end
