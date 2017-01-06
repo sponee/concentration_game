3581fb0f3c53c0052cfdf48cbda93e66904abfdf
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-01-05 16:04:02 -0800

    proper user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105222230) do
+ActiveRecord::Schema.define(version: 20161231021251) do
 
   create_table "cards", force: :cascade do |t|
     t.integer  "game_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170105222230) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "username",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -48,7 +49,6 @@ ActiveRecord::Schema.define(version: 20170105222230) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
