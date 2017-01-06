5b3536547900e08e8c266f46071eba88ee19f99d
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-01-06 13:21:45 -0800

    update development config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,12 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'dry-shelf-95184.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
