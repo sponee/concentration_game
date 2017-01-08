52663668d3391993032554f625600bd5e9743e74
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-01-07 19:16:48 -0800

    better matcher spec

--- spec/models/matcher_spec.rb
+++ spec/models/matcher_spec.rb
@@ -4,22 +4,31 @@ RSpec.describe Matcher do
   before do
     @user = create(:user)
     @game = @user.games.create(player_one_id: @user.id, player_two_id: 2)
-  end
-  it "#compare checks the content of two matchables" do 
-    c1, c2 = @game.cards.where(content: "Pikachu")
-    expect(c1.matched?).to eq(false)
-    expect(c2.matched?).to eq(false)
-    Matcher.compare(c1, c2)
-    expect(c1.matched?).to eq(true)
-    expect(c1.matched?).to eq(true)
+    @c1 = @game.cards.first
+    @c2 = @game.cards.second
   end
 
-  it "#reveal_cards calls #reveal! on its arguments" do
-    c1 = @game.cards.first
-    c2 = @game.cards.second
-    expect(c1).to receive(:reveal!)
-    expect(c2).to receive(:reveal!)
+  it "#comparecalls #reveal_matchables on its arguments" do
+    expect(Matcher).to receive(:reveal_matchables).with([@c1,@c2])
 
-    Matcher.reveal_matchables([c1,c2])
+    Matcher.compare(@c1,@c2)
+  end
+
+  it "#reveal_matchables calls #reveal! on its arguments" do
+    @c1 = @game.cards.first
+    @c2 = @game.cards.second
+    expect(@c1).to receive(:reveal!)
+    expect(@c2).to receive(:reveal!)
+
+    Matcher.reveal_matchables([@c1,@c2])
+  end
+
+  it "#hide_matchables calls hide! on its arguments" do
+    @c1 = @game.cards.first
+    @c2 = @game.cards.second
+    expect(@c1).to receive(:hide!)
+    expect(@c2).to receive(:hide!)
+
+    Matcher.hide_matchables([@c1,@c2])
   end
 end
