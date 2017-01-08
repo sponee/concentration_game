f5d0e4a5c36b54c8e4a7d00f708aef224c997766
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-01-07 18:59:35 -0800

    better matcher specs

--- spec/models/matcher_spec.rb
+++ spec/models/matcher_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Matcher do
+  before do
+    @user = create(:user)
+    @game = @user.games.create(player_one_id: @user.id, player_two_id: 2)
+  end
   it "#compare checks the content of two matchables" do 
-    user = create(:user)
-    game = user.games.create(player_one_id: user.id, player_two_id: 2)
-    c1, c2 = game.cards.where(content: "Pikachu")
+    c1, c2 = @game.cards.where(content: "Pikachu")
     expect(c1.matched?).to eq(false)
     expect(c2.matched?).to eq(false)
     Matcher.compare(c1, c2)
     expect(c1.matched?).to eq(true)
     expect(c1.matched?).to eq(true)
+  end
+
+  it "#reveal_cards calls #reveal! on its arguments" do
+    c1 = @game.cards.first
+    c2 = @game.cards.second
+    expect(c1).to receive(:reveal!)
+    expect(c2).to receive(:reveal!)
+
+    Matcher.reveal_cards([c1,c2])
   end
 end
